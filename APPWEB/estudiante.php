76c1d5523e17ec28131c3c6d05c27821333e52d3
Sebastian Belalcazar Mosquera <sebasbelmosxd@gmail.com>
2024-05-14 20:39:10 -0500

    Update estudiante.php

--- APPWEB/estudiante.php
+++ APPWEB/estudiante.php
@@ -3,9 +3,14 @@
 session_start();
 
 
-// Obtener el ID del usuario autenticado
-$id = $_SESSION["id"];
+// Verificar si el ID está definido en la sesión
+if (!isset($_SESSION["id"])) {
+    // Redirigir al usuario al formulario de inicio de sesión con un mensaje de error
+    header("Location: index.html?error=" . urlencode("No se ha iniciado sesión correctamente."));
+    exit();
+}
 
+$id = $_SESSION["id"];
 
 // URL del microservicio de notas para obtener los datos del usuario
 #$servurl = "http://localhost:3031/notas/$id";
