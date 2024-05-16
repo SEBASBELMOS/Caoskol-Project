035544181245b8a2b2ec99991f90c3e831244723
Sebastian Belalcazar Mosquera <sebasbelmosxd@gmail.com>
2024-05-15 20:07:11 -0500

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -24,7 +24,7 @@ CREATE TABLE usuarios (
 
 INSERT INTO notas (id, nombre, grado, num_premios, programa, puntaje_mat, puntaje_ing, puntaje_ciencias) VALUES
 (2, 'Ana Quintero', 10, 2, 2, 85, 78, 92),
-(3, 'María Fernanda Tello', 11, 0, 1, 90, 82, 88),
+(3, 'Maria Fernanda Tello', 11, 0, 1, 90, 82, 88),
 (4, 'Sebastian Belalcazar', 10, 0, 0, 90, 90, 80),
 (5, 'Lewis Hamilton', 9, 10, 3, 88, 85, 91),
 (6, 'Carlos Sainz', 8, 12, 0, 80, 87, 90);
@@ -32,7 +32,7 @@ INSERT INTO notas (id, nombre, grado, num_premios, programa, puntaje_mat, puntaj
 INSERT INTO usuarios (id, nombre, grado, clave, rol) VALUES
 (1, 'Admin',NULL, 'admin', 'Administrador'),
 (2, 'Ana Quintero', 10, '1234', 'Estudiante'),
-(3, 'María Fernanda Tello', 11, '1234', 'Estudiante'),
+(3, 'Maria Fernanda Tello', 11, '1234', 'Estudiante'),
 (4, 'Sebastian Belalcazar', 10, '1234', 'Estudiante'),
 (5, 'Lewis Hamilton', 9, '1234', 'Estudiante'),
 (6, 'Carlos Sainz', 8, '1234', 'Estudiante');
